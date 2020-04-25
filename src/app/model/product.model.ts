fc184808823baec3fa1e61583108154f039ff0f9
Shivani Jetley <email.shivanijetley@gmail.com>
2020-04-24 19:43:22 -0700

    Added the store and the product components

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,10 @@
+//public also defines the class property
+export class Product {
+  constructor(
+    public id?: number,  
+    public name?: string,
+    public category?: string,
+    public description?: string,
+    public price?: number,
+  ) {}
+}
